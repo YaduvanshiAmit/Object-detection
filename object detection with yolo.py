3afe9ab1365bcd410bb25f190eff49dd954159e5
Yaduvanshi Amit <81853194+YaduvanshiAmit@users.noreply.github.com>
2021-11-28 17:44:14 +0530

    Add files via upload

---
+++ object detection with yolo.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 22 08:09:50 2021
+
+@author: Joel
+"""
+
+import cv2
+import numpy as np
+
+whT = 320 # shape of image which yolo take 
+confThreshold =0.5
+nmsThreshold= 0.2
+
+
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10,150) # britness
+
+
+
+#### LOAD MODEL
+## Coco Names
+classesFile = "coco.names"
+classNames = []
+with open(classesFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+#print(classNames)
+
+## Model Files
+modelConfiguration = "yolov3-tiny.cfg.txt"
+modelWeights = "yolov3-tiny.weights"
+net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+# function for finding object
+def findObjects(outputs,img):
+    hT, wT, cT = img.shape   # image shape
+    bbox = []
+    classIds = []
+    confs = []
+    for output in outputs:
+        for det in output:
+            scores = det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                w,h = int(det[2]*wT) , int(det[3]*hT)
+                x,y = int((det[0]*wT)-w/2) , int((det[1]*hT)-h/2)
+                bbox.append([x,y,w,h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+
+    indices = cv2.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
+ 
+    for i in indices:
+        i = i[0]
+        box = bbox[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+        # print(x,y,w,h)
+        cv2.rectangle(img, (x, y), (x+w,y+h), (255, 0 , 255), 2)
+        cv2.putText(img,f'{classNames[classIds[i]].upper()} {int(confs[i]*100)}%',
+                  (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
+
+
+
+while True:
+    success, img = cap.read()
+    
+    # yolo dont accept image like this we have to convert into blob
+    blob = cv2.dnn.blobFromImage(img, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+    
+    # lets put that into input
+    net.setInput(blob)
+    
+    # nw for output we have to know which which layers it have 
+    # in yolo artictecture there are 3 output 
+    layersNames = net.getLayerNames()
+    #print(layersNames)
+    # by this we get the outputs layerds
+    outputNames = [(layersNames[i[0] - 1]) for i in net.getUnconnectedOutLayers()]
+    outputs = net.forward(outputNames)
+    #print(outputNames) # thsu give the name of output layers
+    #print(outputs)
+    findObjects(outputs,img)
+    
+    
+    cv2.imshow("Image",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
